ad7f688e24f70a36275812266c2e7263592dd1e2
elitonzr <25571917+Elitonzr@users.noreply.github.com>
2021-12-08 20:52:51 -0300

    first commit

---
+++ desafio_area.js
@@ -0,0 +1,8 @@
+// PI * raio * raio
+
+const PI = 3.141592;
+const raio = 10;
+const areaCirculo = PI * raio *raio;
+
+console.log("O valor da área é: " + areaCirculo + "m².");
+console.log(Math.PI);
